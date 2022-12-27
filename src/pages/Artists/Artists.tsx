6c4e7e53e4799b542b1806263a6e039602b050f3
kavispires <kavispires@gmail.com>
2022-12-27 15:08:18 -0800

    Added Placeholder information in /artists and /groups

--- src/pages/Artists/Artists.tsx
+++ src/pages/Artists/Artists.tsx
@@ -3,8 +3,25 @@ import { Chrome } from 'components/Chrome';
 export function Artists() {
   return (
     <Chrome>
-      <div className="artists">
+      <div className="container artists">
         <h2>/artists</h2>
+        <p>3J</p>
+        <p>Leo</p>
+        <p>Bobak</p>
+        <p>Zachary</p>
+        <p>Bryan</p>
+        <p>JQ</p>
+        <p>Nicky</p>
+        <p>Robbie</p>
+        <p>Sypher</p>
+        <p>Nathan</p>
+        <p>Fuduck</p>
+        <p>Diana</p>
+        <p>Olivia</p>
+        <p>Ulla</p>
+        <p>Brittany</p>
+        <p>Tracy</p>
+        <p>... Relevants contestants</p>
       </div>
     </Chrome>
   );
--- src/pages/Groups/Groups.tsx
+++ src/pages/Groups/Groups.tsx
@@ -3,8 +3,13 @@ import { Chrome } from 'components/Chrome';
 export function Groups() {
   return (
     <Chrome>
-      <div className="groups">
+      <div className="container groups">
         <h2>/groups</h2>
+        <p>Squirrel Zone</p>
+        <p>Squirrel Zone II</p>
+        <p>Doubt Girls</p>
+        <p>Squirrel Zone OT4</p>
+        <p>Squirrel Zone with Fuduck</p>
       </div>
     </Chrome>
   );
