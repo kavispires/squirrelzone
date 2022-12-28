7ac1ee6bf14a7e823101748495022aef1108f927
kavispires <kavispires@gmail.com>
2022-12-27 17:18:21 -0800

    Added Artists dna for SQI

--- src/pages/Artists/Artists.tsx
+++ src/pages/Artists/Artists.tsx
@@ -1,15 +1,22 @@
 import { Chrome } from 'components/Chrome';
+import { SquirrelAvatar } from 'components/SquirrelAvatar';
+import './Artists.scss';
 
 export function Artists() {
   return (
     <Chrome>
       <div className="container artists">
         <h2>/artists</h2>
-        <p>3J</p>
-        <p>Leo</p>
-        <p>Bobak</p>
-        <p>Zachary</p>
-        <p>Bryan</p>
+        <div className="members">
+          {squirrelZoneI.map((member) => {
+            return (
+              <div className="member" key={member.name}>
+                <SquirrelAvatar dna={member.dna} color={member.color} />
+                <span className="member__name">{member.name}</span>
+              </div>
+            );
+          })}
+        </div>
         <p>JQ</p>
         <p>Nicky</p>
         <p>Robbie</p>
@@ -26,3 +33,51 @@ export function Artists() {
     </Chrome>
   );
 }
+
+const squirrelZoneI = [
+  {
+    name: '3J',
+    dna: '0:0:0:15:1:4:1:0:0:0:0:0:0:0:0:0:0:0',
+    color: {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    },
+  },
+  {
+    name: 'Leo',
+    dna: '0:1:0:12:2:1:10:0:0:4:9:4:0:0:0:0:0:0',
+    color: {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    },
+  },
+  {
+    name: 'Bobak',
+    dna: '0:3:0:0:0:9:11:0:0:3:4:2:0:0:0:0:0:0',
+    color: {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    },
+  },
+  {
+    name: 'Zachary',
+    dna: '2:2:2:5:0:12:13:0:0:2:5:3:0:0:0:0:0:0',
+    color: {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    },
+  },
+  {
+    name: 'Bryan',
+    dna: '1:2:1:0:4:6:5:0:0:1:10:1:0:0:0:0:0:0',
+    color: {
+      hue: 0,
+      saturation: 0,
+      lightness: 0,
+    },
+  },
+];
